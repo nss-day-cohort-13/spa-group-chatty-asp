ffca310415176d12c17465622fa88b42518fbffc
slick <slick@Australopithecus.local>
2016-05-03 15:38:11 -0500

    delete themes.js

--- javascripts/themes.js
+++
@@ -1,9 +0,0 @@
-//**********//XHR//**********//
-	
-var chatty = function(object) {
-	
-	//RETURNS AN OBJECT WITH A METHOD ATTACHED//	
-	return object;	
-
-//INVOKES FUNCTION WITH EMPTY OBJECT IF LIST MUSIC HAS NOT YET INITIALIZED//
-}(chatty || {});
