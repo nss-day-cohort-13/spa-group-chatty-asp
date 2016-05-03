b192cc4b622cc2bdd49f7add8c330b56470976de
Amari Ashwood <fromashestosnow@gmail.com>
2016-05-03 15:29:38 -0500

    renamed themes to options

---
+++ javascripts/options.js
@@ -0,0 +1,9 @@
+//**********//XHR//**********//
+	
+var chatty = function(object) {
+	
+	//RETURNS AN OBJECT WITH A METHOD ATTACHED//	
+	return object;	
+
+//INVOKES FUNCTION WITH EMPTY OBJECT IF LIST MUSIC HAS NOT YET INITIALIZED//
+}(chatty || {});
