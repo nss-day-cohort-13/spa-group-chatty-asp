4aee1419d9757a5c7a9685171bef7f25e820bca8
Amari Ashwood <fromashestosnow@gmail.com>
2016-05-03 15:55:15 -0500

    added dark theme function

--- javascripts/options.js
+++ javascripts/options.js
@@ -1,9 +1,20 @@
 //**********//XHR//**********//
-	
+
 var chatty = function(object) {
-	
-	//RETURNS AN OBJECT WITH A METHOD ATTACHED//	
-	return object;	
+
+	var darkThemeCheck = $("#darkTheme");
+	darkThemeCheck.click(darkify);
+
+	function darkify () {
+ 	 webpage.toggleClass("makeDark"); //"makeDark" is coming from the CSS
+  }
+
+  //RETURNS AN OBJECT WITH A METHOD ATTACHED//
+  return object;  //guys, I'm not sure what this should return-AAA
+
 
 //INVOKES FUNCTION WITH EMPTY OBJECT IF LIST MUSIC HAS NOT YET INITIALIZED//
-}(chatty || {});
+}(chatty || {}); //guys, what should go here?-AAA
+
+
+
